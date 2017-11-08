1d21b76dfe03ede500ecb81003b07458829d53b6
Alex Chubaty <alex.chubaty@gmail.com>
2017-11-08 12:00:28 -0700

    add modules-version-check-rezip.R
    moved out of lab repo to here (with https://github.com/eliotmcintire/McIntire-lab/commit/a986a7a6cbd86bc1eeb9278f4c6b8b267dd31664)

---
+++ modules-version-check-rezip.R
@@ -0,0 +1,40 @@
+library(igraph)
+library(SpaDES)
+
+setwd("~/Documents/GitHub/SpaDES-modules")
+
+# check all module versions, and rezip if needed
+omit <- ".Rproj.user"
+
+allModules <- list.dirs("modules/", recursive = FALSE) %>% basename()
+currentModules <- if (length(omit)) {
+  allModules[-which(allModules %in% omit)]
+} else {
+  allModules
+}
+rezipped <- list()
+
+sim <- simInit() ## workaround: need dummy simList to get metadata below
+out <- lapply(currentModules, function(x) {
+  version <- SpaDES.core::moduleVersion(module = x, path = "modules") %>% as.character()
+  zipFile <- paste0("modules/", x, "/", x, "_", version, ".zip")
+  if (!file.exists(zipFile)) {
+    SpaDES.core::zipModule(x, "modules")
+    rezipped <<- append(rezipped, x)
+  }
+})
+rezipped <- unlist(rezipped)
+
+## status messages
+m <- list(omit = omit, checked = currentModules, rezipped = rezipped)
+out <- lapply(names(m), function(x) {
+  m[[x]] <<- if ( is.null(m[[x]]) | length(m[[x]]) == 0 ) {
+      "none"
+  } else {
+      m[[x]]
+  }
+})
+
+message("Modules omitted:\n\t", paste(m$omit, collapse = "\n\t"))
+message("Modules checked:\n\t", paste(m$checked, collapse = "\n\t"))
+message("Modules rezipped:\n\t", paste(m$rezipped, collapse = "\n\t"))
