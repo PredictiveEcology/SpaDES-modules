262bf7ff46949f02b7a7f853f6476ce25d99e441
CeresBarros <cbarros@mail.ubc.ca>
2018-02-04 20:21:27 -0800

    minor changes to module zipping script

--- modules-version-check-rezip.R
+++ modules-version-check-rezip.R
@@ -7,18 +7,22 @@ setwd("~/Documents/GitHub/SpaDES-modules")
 omit <- ".Rproj.user"
 
 allModules <- list.dirs("modules/", recursive = FALSE) %>% basename()
-currentModules <- if (length(omit)) {
-  allModules[-which(allModules %in% omit)]
+currentModules <- if(length(omit)) {
+  allModules[!allModules %in% omit]
 } else {
   allModules
 }
 rezipped <- list()
 
 sim <- simInit() ## workaround: need dummy simList to get metadata below
+
+## overwrite existing zip?
+overwrite = TRUE
+
 out <- lapply(currentModules, function(x) {
   version <- SpaDES.core::moduleVersion(module = x, path = "modules") %>% as.character()
   zipFile <- paste0("modules/", x, "/", x, "_", version, ".zip")
-  if (!file.exists(zipFile)) {
+  if(!file.exists(zipFile) | overwrite == TRUE) {
     SpaDES.core::zipModule(x, "modules")
     rezipped <<- append(rezipped, x)
   }
