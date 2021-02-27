915a0fe8f21eb112084aa33af7a32c8d866360b0
Eliot McIntire <eliotmcintire@gmail.com>
2021-02-26 16:26:26 -0800

    makeSureAllPackagesInstalled

--- R/SpaDES_Helpers.R
+++ R/SpaDES_Helpers.R
@@ -147,3 +147,60 @@ installSpaDES <- function(ask = FALSE, type, libPath = .libPaths()[1],
   }
   return(invisible())
 }
+
+#' Pre-test for packages in SpaDES modules
+#'
+#' This function can be put at the start of project code. It will
+#' only be necessary if there are multiple \code{simInit} calls (i.e., the project
+#' doesn't use only one call). It will check all modules in \code{modulePath} for
+#' package dependencies. It will prompt
+#' @param modulePath The path to modules, as per \code{SpaDES.core::setPaths}
+#' @examples
+#' \dontrun{
+#' source("https://raw.githubusercontent.com/PredictiveEcology/SpaDES-modules/master/R/SpaDES_Helpers.R")
+#' out <- makeSureAllPackagesInstalled(modulePath = "modules")
+#' }
+makeSureAllPackagesInstalled <- function(modulePath) {
+  AllPackagesFile <- "._AllPackages.rds"
+  if (!file.exists(AllPackagesFile)) {
+    AllModules <- dir(modulePath)
+    AllPackages <- lapply(AllModules, function(mod) {
+      print(mod)
+      SpaDES.core::packages(modules = mod, paths = modulePath)
+    })
+
+    AllPackagesUnlisted <- unname(unlist(AllPackages))
+    out <- Require::Require(require = FALSE, AllPackagesUnlisted, install = FALSE, verbose = TRUE)
+    out <- attr(out, "Require")
+    okVersions <- Require::getPkgVersions(out)
+    okInstalled <- all(out$installed)
+    okVersion <- all(okVersions$compareVersion >= 0)
+    data.table::setorderv(out, "Package")
+    data.table::setnames(out, old = "Version", "InstalledVersion")
+    colsToShow <- c("packageFullName", "Package", "InstalledVersion", "correctVersion")
+    out <- out[compareVersion <0, ..colsToShow]
+
+    uniquedPkgs <- unique(out$Package)
+    anyLoaded <- vapply(uniquedPkgs, function(pkg) isNamespaceLoaded(pkg), FUN.VALUE = logical(length(uniquedPkgs)))
+
+    if (!all(okVersion & okInstalled & anyLoaded)) {
+      obj <- list(state = out, AllPackagesUnlisted = AllPackagesUnlisted)
+      saveRDS(obj, file = AllPackagesFile)
+      message("The following packages are in an incorrect state: ")
+      reproducible::messageDF(print(out))
+      stop("Restart R; Run this function again immediately.", call. = FALSE)
+    }
+  } else {
+    AllPackagesUnlisted <- readRDS(AllPackagesFile)
+    uniquedPkgs <- unique(AllPackagesUnlisted$state$Package)
+    anyLoaded <- vapply(uniquedPkgs, function(pkg) isNamespaceLoaded(pkg), FUN.VALUE = logical(length(uniquedPkgs)))
+
+    if (anyLoaded)
+      stop("Some packages that need to be updated are still loaded; please restart R.",
+           "You may have to change your settings so packages don't get automatically loaded")
+    Require::Require(require = FALSE, AllPackagesUnlisted$AllPackagesUnlisted, upgrade = FALSE)
+    unlink(AllPackagesFile)
+  }
+  message("All packages correctly installed and correct versions")
+}
+
